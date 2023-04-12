0c7291f846e5f28d8452d1685071d1812a8a4370
Victor Guerra <vguerra@gmail.com>
2023-04-12 22:28:36 +0200

    Adding a bit more of structure.
    Modifying the nav bar and the theme.
    Experimenting with structure for posts, tils and notes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,13 @@ pip install nbdev black
 nbdev_new --lib_name vguerra-blog \
 	--nbs_path "./nbs" \
 	--git_url "https://github.com/vguerra/www" \
-	--title "Victor Guerra's blog" \
+	--title "Blog" \
 	--repo "www" \
 	--branch "main" \
 	--user "vguerra" \
 	--author "Victor Guerra" \
 	--author_email "vguerra@gmail.com" \
-	--description "Victor Guerra's blog posts" \
+	--description "Victor Guerra's blog" \
 	--black_formatting true
 
 
